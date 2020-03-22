4b5128f8d207ccaf42e4707991c79a9e03e048bf
adalbert44 <adalbert.makarovych@gmail.com>
2020-03-22 11:33:45 +0200

    Added diagrams

---
+++ Controllers/RestaurantsDiagramController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantsDiagramController : ControllerBase
+    {
+        private readonly FoodDelivery_v2Context _context;
+
+        public RestaurantsDiagramController(FoodDelivery_v2Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("JsonData")]
+        public JsonResult JsonData()
+        {
+            var restaurants = _context.Restaurant.Include(b => b.RestaurantLocation).ToList();
+
+            List<object> catRestaurantLocations = new List<object>();
+            catRestaurantLocations.Add(new[] { "Ресторан", "Кількість закладів" });
+
+            foreach (var r in restaurants)
+            {
+                catRestaurantLocations.Add(new object[] { r.Name, r.RestaurantLocation.Count() });
+            }
+
+            return new JsonResult(catRestaurantLocations);
+        }
+    }
+}
---
+++ Controllers/TypesDiagramController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TypesDiagramController : ControllerBase
+    {
+        private readonly FoodDelivery_v2Context _context;
+
+        public TypesDiagramController(FoodDelivery_v2Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("JsonData")]
+        public JsonResult JsonData()
+        {
+            var types = _context.Type.Include(b => b.Dish).ToList();
+
+            List<object> catDishes = new List<object>();
+            catDishes.Add(new[] { "Тип", "Кількість страв" });
+
+            foreach (var t in types)
+            {
+                catDishes.Add(new object[] { t.Name, t.Dish.Count() });
+            }
+
+            return new JsonResult(catDishes);
+        }
+
+    }
+}
